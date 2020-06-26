df9f2dfb560f8042cd12c721011f9e92e37612be
Simone Vittori <svitto@svit.to>
2020-06-26 12:51:16 +0100

    Description shouldn't end with a full stop

--- Formula/cindy.rb
+++ Formula/cindy.rb
@@ -1,5 +1,5 @@
 class Cindy < Formula
-  desc "🌼 A command-line newsletter sender, written in Go."
+  desc "🌼 A command-line newsletter sender, written in Go"
   homepage "https://github.com/simonewebdesign/cindy"
   url "https://github.com/simonewebdesign/cindy/archive/v1.0.0.tar.gz"
   sha256 "a4f71c092a0f0e2164d2f6ebb40d2a2959401f15faa5413c2c738797d0b555fc"
