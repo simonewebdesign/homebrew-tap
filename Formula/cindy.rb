d8c75a59fe64a3898c7808a5486bf4d9318fcdea
Simone Vittori <svitto@svit.to>
2020-06-28 19:28:19 +0100

    Update cindy formula

--- Formula/cindy.rb
+++ Formula/cindy.rb
@@ -1,8 +1,9 @@
 class Cindy < Formula
   desc "🌼 A command-line newsletter sender, written in Go"
   homepage "https://github.com/simonewebdesign/cindy"
-  url "https://github.com/simonewebdesign/cindy/archive/v1.0.0.tar.gz"
-  sha256 "a4f71c092a0f0e2164d2f6ebb40d2a2959401f15faa5413c2c738797d0b555fc"
+  url "https://github.com/simonewebdesign/cindy/releases/download/v2.0.0/cindy_2.0.0_darwin_amd64.tar.gz"
+  version "2.0.0"
+  sha256 "08665d1f79215d6e018acf1d98ce6d81d0d6cd03899b490e88993291cca2c9e6"
 
   def install
     bin.install "cindy"
